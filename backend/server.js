b9bf562bef43ec5436c6bff4002d5eef08a9a94a
Jeet Srivastava <jeetsrivastava1706@gmail.com>
2026-03-01 11:58:31 +0530

    feat: integrated mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
@@ -13,8 +15,10 @@ app.get('/', (req, res) => {
     res.send('peer2peer API is running...');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+
