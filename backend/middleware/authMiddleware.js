790ca4ce0aa7bfd10f06a8606749820c5116a1de
Jeet Srivastava <jeetsrivastava1706@gmail.com>
2026-03-01 14:03:56 +0530

    JWT auth

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const protect = async (req, res, next) => {
+    let token;
+
+    //authorization header + Bearer prefix check
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            //auth header se token lena
+            token = req.headers.authorization.split(' ')[1];
+
+            //verifying the token
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            //ID se user nikalke aur request me set kara
+            req.user = await User.findById(decoded.id).select('-password');
+
+            next();
+        } catch (error) {
+            console.error(error);
+            res.status(401).json({ message: 'Not authorized, token failed' });
+        }
+    }
+
+    if (!token) {
+        res.status(401).json({ message: 'Not authorized, no token' });
+    }
+};
+
+module.exports = { protect };
